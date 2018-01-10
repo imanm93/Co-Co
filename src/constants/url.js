5dcc4ed942b3f18e07712a3366fd75a18be909a2
Malin <szyszka45@gmail.com>
2018-01-10 13:02:18 +0000

    https to http

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,9 +1,9 @@
 
-// let ROOT_URL = 'http://localhost:5000';
+//let url = 'http://localhost:5000';
 
 // let url = 'http://coandcoapi.azurewebsites.net';
 
-let url = 'https://coandcotestprod.azurewebsites.net';
+let url = 'http://coandcotestprod.azurewebsites.net';
 
 if (process.env.NODE_ENV === "production") {
     url = 'http://coandcoapi.azurewebsites.net'
