71e967ee3ff3e544339d237d3db692faecd21d1b
Malin <szyszka45@gmail.com>
2018-01-27 22:54:52 +0000

    change url

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,9 +1,9 @@
 
-//let url = 'http://localhost:5000';
+let url = 'http://localhost:5000';
 
 // let url = 'http://coandcoapi.azurewebsites.net';
 
-let url = 'http://coandcotestprod.azurewebsites.net';
+//let url = 'https://coandcotest.azurewebsites.net';
 
 if (process.env.NODE_ENV === "production") {
     url = 'http://coandcoapi.azurewebsites.net'
